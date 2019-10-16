a36473ccf8288a69e7810cea4ed2c82b57554406
Nicholas Grout <njgrout@gmail.com>
2019-10-16 16:07:53 -0700

    new file:   .clang-tidy 	modified:   CMakeLists.txt 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "./src/span.hpp"
 #include <iostream>
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
   auto span = nstd::span<int>();
-
   std::cout << "setup" << std::endl;
 }
